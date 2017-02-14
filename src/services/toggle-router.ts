c81c048eccebe7ccbc9e12a99aae569a94590c03
ttmarek <ttmarek@outlook.com>
2017-02-14 12:04:29 -0500

    Comment out router tests

--- src/services/toggle-router.ts
+++ src/services/toggle-router.ts
@@ -1,26 +1,29 @@
-// doesn't work when running tests
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { NgRedux } from '@angular-redux/store';
 
+import { IAppState } from '../store/store';
 import { featureToggleConfigs } from '../toggle-config';
-
-import {FeatureToggleActions} from '../actions/feature-toggle.actions';
-import {Injectable} from '@angular/core';
-import {Observable} from 'rxjs';
-import {IAppState} from '../store/store';
-import {NgRedux} from '@angular-redux/store';
-import {IFeatureToggleConfig, IFeatureToggleConfigSet} from '../toggle-config/toggle.config.types';
-import {IToggleRecord} from '../store/feature-toggle/feature-toggle.types';
+import { FeatureToggleActions } from '../actions/feature-toggle.actions';
+import { IToggleRecord } from '../store/feature-toggle/feature-toggle.types';
+import {
+  IFeatureToggleConfig,
+  IFeatureToggleConfigSet,
+} from '../toggle-config/toggle.config.types';
 
 @Injectable()
 export class ToggleRouter {
-  private static configs : IFeatureToggleConfigSet = featureToggleConfigs;
-  private toggleConfig$ : Observable<IToggleRecord> ;
-  private toggleRecord : IToggleRecord;
+  private static configs: IFeatureToggleConfigSet = featureToggleConfigs;
+  private toggleConfig$: Observable<IToggleRecord>;
+  private toggleRecord: IToggleRecord;
+
   constructor(
     private ngRedux: NgRedux<IAppState>,
-    private featureToggleActions : FeatureToggleActions) {
-    this.toggleConfig$ =  // TODO This cast might be dangerous
-      <Observable<IToggleRecord>>this.ngRedux.select('toggles');
-    this.toggleConfig$.subscribe( (toggleRecord : IToggleRecord) => {
+    private featureToggleActions: FeatureToggleActions
+  ) {
+    // TODO This cast might be dangerous
+    this.toggleConfig$ = <Observable<IToggleRecord>>this.ngRedux.select('toggles');
+    this.toggleConfig$.subscribe((toggleRecord: IToggleRecord) => {
       this.toggleRecord = toggleRecord;
     });
   }
@@ -31,10 +34,10 @@ export class ToggleRouter {
       const config : IFeatureToggleConfig = ToggleRouter.configs[key];
       initialState[key] = config.setting;
     });
-
     return initialState;
   }
-  setFeatureState(featureState) : void {
+
+  setFeatureState(featureState): void {
     this.featureToggleActions.toggleFeatureSetting(featureState);
   }
 
@@ -42,12 +45,11 @@ export class ToggleRouter {
     return this.toggleRecord.get(toggleId);
   }
 
-  watch (toggleId : string) : Observable<any> {
+  watch(toggleId: string): Observable<any> {
     // TODO: This will fire whenever any toggle changes,
     // fix it so it only fires when needed
-    return this.toggleConfig$.map( (configSet : IToggleRecord) => {
+    return this.toggleConfig$.map((configSet: IToggleRecord) => {
       return configSet.get(toggleId);
-    } );
-
+    });
   }
 }
