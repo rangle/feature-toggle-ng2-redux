c613ca67491daa35bd96e5a375eafe48419122d3
Ken Ono <ken.ono.gm@gmail.com>
2017-02-20 19:28:30 -0500

    WIP personalization

--- src/app/sample-app.module.ts
+++ src/app/sample-app.module.ts
@@ -1,6 +1,7 @@
 import {NgModule}      from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {BrowserModule} from '@angular/platform-browser';
+import {HttpModule} from '@angular/http';
 import {NgReduxModule, DevToolsExtension } from '@angular-redux/store';
 import {NgReduxRouterModule} from '@angular-redux/router';
 import {
@@ -17,39 +18,47 @@ import {SessionActions, FeatureToggleActions} from '../actions';
 import {SessionEpics} from '../epics/session.epics';
 import {
   RioAboutPage,
-  RioCounterPage
+  RioCounterPage,
+  RioPersonalizedPage
 } from '../pages';
 import {RioCounter} from '../components/counter/counter.component';
 import {RioLoginModule} from '../components/login/login.module';
 import {RioUiModule} from '../components/ui/ui.module';
 import {RioModalModule} from '../components/modal/modal.module';
 import {RioNavigatorModule} from '../components/navigator/navigator.module';
+import { RioCallToAction } from '../components/call-to-action/call-to-action';
+import { RioFormModule } from '../components/form/form.module';
 
 import {
   ShowIfFeatureDirective
 } from '../directives/';
 import {ToggleRouter} from '../services/toggle-router';
+import {IPInfo} from '../services/ip-info';
 
 @NgModule({
   imports: [
     FormsModule,
     ReactiveFormsModule,
     BrowserModule,
+    HttpModule,
     routing,
     CommonModule,
     RioLoginModule,
     RioUiModule,
+    RioFormModule,
     RioNavigatorModule,
     RioModalModule,
     NgReduxModule,
-    NgReduxRouterModule,
+    NgReduxRouterModule
   ],
   declarations: [
     RioSampleApp,
     RioAboutPage,
     RioCounterPage,
+    RioPersonalizedPage,
     RioCounter,
-    ShowIfFeatureDirective
+    ShowIfFeatureDirective,
+    RioCallToAction
   ],
   bootstrap: [
     RioSampleApp
@@ -60,7 +69,8 @@ import {ToggleRouter} from '../services/toggle-router';
     SessionActions,
     SessionEpics,
     FeatureToggleActions,
-    ToggleRouter
+    ToggleRouter,
+    IPInfo
   ]
 })
 export class RioSampleAppModule { }
