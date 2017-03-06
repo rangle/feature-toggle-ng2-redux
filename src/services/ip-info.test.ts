85c9ba3d8b4cee0c46430fd42bf244306593f59d
Ken Ono <ken.ono.gm@gmail.com>
2017-03-06 15:31:55 -0500

    cosemetic fixes and fix tests

--- src/services/ip-info.test.ts
+++ src/services/ip-info.test.ts
@@ -1,10 +1,13 @@
 import { Observable } from 'rxjs';
 import { IPInfo, IIPInfo } from './ip-info';
-import { Http, BaseRequestOptions, ConnectionBackend, XHRBackend, BrowserXhr, ResponseOptions, XSRFStrategy, HttpModule }          from '@angular/http';
+import {
+  Http, BaseRequestOptions,
+  XHRBackend,
+  HttpModule
+} from '@angular/http';
 import { TestBed, inject } from '@angular/core/testing';
 
 describe('ip-info', () => {
-  let watcher;
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
@@ -18,25 +21,19 @@ describe('ip-info', () => {
           deps: [BaseRequestOptions]
         }, XHRBackend ]
       });
-    watcher = {
-      callCheck: () => {}
-    };
-
-    spyOn(watcher, 'callCheck');
   });
 
   it('should have functioning demo data so the demo IPs return the expected countries. If the serivce breaks, test should fail',
-
-    inject([IPInfo,Http], (ipInfo, http) => {
-      ipInfo = new IPInfo(http);
-      const testIP = '69.17.177.26';
-      let ipInfo$ : Observable<IIPInfo> = ipInfo.getIPInfo(testIP);
-      ipInfo$.subscribe( (iipInfo : IIPInfo) =>{
-        watcher.callCheck();
-//        expect(iipInfo.country).toEqual('CA');
-      } );
-
-//      expect(watcher.callCheck).toHaveBeenCalled();
-    })
+    (done)=>{
+      inject([IPInfo,Http], (ipInfo, http) => {
+        ipInfo = new IPInfo(http);
+        const testIP = '69.17.177.26';
+        let ipInfo$: Observable<IIPInfo> = ipInfo.getIPInfo(testIP);
+        ipInfo$.subscribe( (iipInfo: IIPInfo) =>{
+          expect(iipInfo.countryCode).toEqual('CA');
+          done();  // Ensures this was run
+        } );
+      })(); // Invoke function returned by inject
+    }
   );
 });
